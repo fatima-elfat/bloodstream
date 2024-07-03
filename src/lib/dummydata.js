2972454c1ad034a0b0b4390a55471e779072b7b5
fatima-elfat <ffzelfatehy@gmail.com>
2024-07-04 00:30:53 +0100

    Frontnd: add data to show

---
+++ src/lib/dummydata.js
@@ -0,0 +1,41 @@
+export const listData = [
+  {
+    id: 141555,
+    title: "Blood Center Mohammed VI - Hôpital Universitaire International Mohammed VI de Bouskoura.",
+    type: ["A+", "O+"],
+    level: "high",
+    nbr: 5,
+    address: "P3011, Bouskoura 27182",
+    latitude: 33.4689234,
+    longitude: -7.6427658,
+  },
+  {
+    id: 141555,
+    title: "Blood Center Mohammed VI - Hôpital Universitaire International Cheikh Khalifa.",
+    type: ["AB+", "B+"],
+    level: "high",
+    nbr: 6,
+    address: "Hopital Cheikh Khalifa_, Casablanca",
+    latitude: 33.5513179,
+    longitude: -7.6689626,
+  },
+];
+
+export const singlePostData = {
+  id: 141555,
+  title: "Blood Center Mohammed VI - Hôpital Universitaire International Mohammed VI de Bouskoura.",
+  type: ["A+", "O+"],
+  level: "high",
+  nbr: 6,
+  address: "P3011, Bouskoura 27182",
+  latitude: 33.4689234,
+  longitude: -7.6427658,
+  address: "P3011, Bouskoura 27182",
+  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos explicabo suscipit cum eius, iure est nulla animi consequatur facilis id pariatur fugit quos laudantium temporibus dolor ea repellat provident impedit!",
+  };
+
+export const userData = {
+  id: 1,
+  name: "John Doe",
+  img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+};
